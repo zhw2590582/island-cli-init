d2b3713dd25eaf41310cc61960a2cfa42cb44858
Harvey Zack <laozhaochaguan@gmail.com>
2018-08-23 14:38:34 +0800

    bugfix

--- src/data/config/wrap.js
+++ src/data/config/wrap.js
@@ -1,17 +1,17 @@
 module.exports = function(config) {
-    config.menus = config.menu
+    config.web.menus = config.web.menus
       .map(item => {
         return `<a class="item" title="${item.name}" href="${item.link}">${item.name}</a>`;
       })
       .join("");
   
-    config.socials = config.socials
+    config.web.socials = config.web.socials
       .map(item => {
         return `<a class="social-item" title="${item.name}" href="${item.link}" target="_blank">${item.name}</a>`;
       })
       .join('<span class="dot"></span>');
   
-    config.links = config.links
+    config.web.links = config.web.links
       .map(item => {
         return `<a class="link-item" title="${item.name}" href="${item.link}">${item.name}</a>`;
       })
